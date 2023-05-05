ca9e91c3b3df83e49738cd9e7290fa4654a79983
Maria Rios Navarro <propscode254@gmail.com>
2023-05-05 14:02:10 +0200

    delete everythin and beginn another time

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// phew… not a lot going on here. Please add some code!
 
-console.log("hello");
 
-const bookmark = document.querySelector('[data-js="bookmark-button"]');
 
-bookmark.addEventListener("click", () => {
-  bookmark.classList.toggle("bookmark--active");
-});
-
-console.log("hello");
-
-const answer = document.querySelector('[data-js="answer"]');
