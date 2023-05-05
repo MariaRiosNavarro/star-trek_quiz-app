344f751104718bc02853ad6eda76c3559113dbe3
Maria Rios Navarro <propscode254@gmail.com>
2023-05-05 12:42:57 +0200

    add second const

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // phew… not a lot going on here. Please add some code!
 
+const bookmark = document.querySelector('[data-js="bookmark-button"]');
 
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("bookmark--active");
+});
+
+const answer = document.querySelector('[data-js="answer"]');
