8aef937fb1f083f744e9c6eb4ec0f8662d844a36
Maria Rios Navarro <propscode254@gmail.com>
2023-05-05 16:17:13 +0200

    add js toggle

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const bookmark = document.querySelector('[data-js="bookmark-button"]');
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const answer = document.querySelector('[data-js="answer"]');
 
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("bookmark--active");
+});
 
-
+answerButton.addEventListener("click", () => {
+  answer.classList.toggle("card__answer--active");
+});
