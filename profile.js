ee24dd339babfb1a878df29e4f6d85c584887271
Maria Rios Navarro <propscode254@gmail.com>
2023-05-05 16:55:00 +0200

    add toggle in user for nav

--- profile.js
+++ profile.js
@@ -4,10 +4,12 @@ const darkModeCheckbox = document.querySelector(
 
 const main = document.querySelector('[data-js="main"]');
 const header = document.querySelector('[data-js="header"]');
+const nav = document.querySelector('[data-js="nav"]');
 
 darkModeCheckbox.addEventListener("click", () => {
   main.classList.toggle("dark");
   header.classList.toggle("darkblue");
+  nav.classList.toggle("darkblue");
 });
 
-console.log("hello2");
+// to do: icons light
