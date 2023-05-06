dbe2b67e2369db0dec5a60867e2c90d96a457a48
Maria Rios Navarro <propscode254@gmail.com>
2023-05-06 15:51:38 +0200

    Add js for Show/Hide Button

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+//create Varibles from HTML
+
 const bookmark = document.querySelector('[data-js="bookmark-button"]');
 const answerButton = document.querySelector('[data-js="answer-button"]');
 const answer = document.querySelector('[data-js="answer"]');
+
+//Event Listener for Bookmark
 
 bookmark.addEventListener("click", () => {
   bookmark.classList.toggle("bookmark--active");
 });
 
+//Event Listener for Buttons and text change in Button
+
 answerButton.addEventListener("click", () => {
   answer.classList.toggle("card__answer--active");
-  answerButton.textContent.toggle = "Hide answer";
+  if (answer.classList.contains("card__answer--active")) {
+    answerButton.textContent = "Hide Answer";
+  } else {
+    answerButton.textContent = "Show Answer";
+  }
 });
