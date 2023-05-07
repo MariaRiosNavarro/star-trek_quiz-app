cf3b4ab573a738329d56236817ec0573136cb284
Maria Rios Navarro <propscode254@gmail.com>
2023-05-07 19:25:06 +0200

    change style bookmarks and svg for darkmode

--- components/profile/profile.js
+++ components/profile/profile.js
@@ -6,10 +6,16 @@ const main = document.querySelector('[data-js="main"]');
 const header = document.querySelector('[data-js="header"]');
 const nav = document.querySelector('[data-js="nav"]');
 
+const svgindex = document.querySelector('[data-js="svgindex"]');
+const svgbookmarkt = document.querySelector('[data-js="svgbookmarks"]');
+const svgform = document.querySelector('[data-js="svgform"]');
+const svgprofile = document.querySelector('[data-js="svgprofile"]');
+
 darkModeCheckbox.addEventListener("click", () => {
   main.classList.toggle("dark");
   header.classList.toggle("darkblue");
   nav.classList.toggle("darkblue");
+  svgindex.classList.toggle("yellowIconDarkMode");
+  svgbookmarkt.classList.toggle("yellowIconDarkMode");
+  svgform.classList.toggle("yellowIconDarkMode");
 });
-
-// to do: icons light
